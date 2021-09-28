c1e2419c9b87e5974dd38faca6c10e1c5cc50293
apul8377 <apulvers@my.chemeketa.edu>
2021-09-28 09:49:01 -0700

    started configuring GameWinCondition and Button

--- src/Button.js
+++ src/Button.js
@@ -1,33 +1,49 @@
 import { useState } from "react";
 
-const checkXOrO = (getSquare) => {
-  if (getSquare === "X") {
+const checkXOrO = (currentPlayer) => {
+  if (currentPlayer === "player2") {
     return "O";
   }
   return "X";
 };
 
-const Button = ({ clickedSquare, xOrOStatus }) => {
-  const [xOrO, setXOrO] = useState(null);
-  xOrOStatus = checkXOrO(xOrOStatus);
-  
-  const isXOrO = (xOrO) => {
-      const nextChoice = checkXOrO(xOrO);
-      setXOrO(nextChoice);
-    };
-    
-    const buttonText = (xOrO) => {
-        if(xOrO === null){
-            return " ";
-        }
-        return xOrO;
-    };
-    const showButton = buttonText(xOrO);
-  const handleClick = () => {
-    isXOrO();
 
+const Button = ({ player, id, xOrOStatus }) => {
+  const [xOrO, setXOrO] = useState(null);
+  const [clickCount, setClickCount] = useState(0);
+  //const [playerState, setPlayerState] = useState("player1");
+  const targetButton = id;
+  const playerStatus = player === "player1" ? "player2" : "player1";
+  
+
+
+  const isXOrO = (playerStatus) => {
+    const nextChoice = checkXOrO(playerStatus);
+    setXOrO(nextChoice);
   };
-  console.log(buttonText());
+
+  const buttonText = (xOrO) => {
+    if (xOrO === null) {
+      checkXOrO(xOrO);
+    }
+    return xOrO;
+  };
+  
+  const showButton = buttonText(xOrO);
+
+  const handleCount = () => {
+      let count = 0;
+      count += 1;
+      setClickCount(count);
+  }
+
+  const handleClick = () => {
+        isXOrO();
+        console.log(targetButton);
+        console.log(playerStatus);
+    };
+
+
   return (
     <button
       onClick={handleClick}
@@ -45,4 +61,3 @@ const Button = ({ clickedSquare, xOrOStatus }) => {
 };
 
 export default Button;
-
--- src/GameWinCondition.js
+++ src/GameWinCondition.js
@@ -1,14 +1,112 @@
 import { useState } from "react";
 import Button from "./Button";
 
-const GameWinCondition = ( { player, isGameOver, gameState, clickedSquare,  }) => {
+const checkXOrO = (getSquare) => {
+  if (getSquare === "X") {
+    return "O";
+  }
+  return "X";
+};
 
-  return(
+const changePlayer = (newPlayer) => {
+  if(newPlayer === "player1"){
+      return "player2"
+  }
+  return "player1"
+};
+
+const GameWinCondition = ({ Activeplayer, isGameOver, clickedSquare }) => {
+  const [clickState, setClickState] = useState(null);
+  const [currentPlayer, setCurrentPlayer] = useState("player1");
+  const [gameState, setGameState] = useState([" ", " ", " ", " ", " ", " ", " ", " ", " ", ]);
+  
+  const getNewPlayer = () => {
+    const newPlayer = changePlayer(currentPlayer);
+    setCurrentPlayer(newPlayer);
+};
+
+  const row1 = [];
+  const row2 = [];
+  const row3 = [];
+  for(let i=0; i<3; i++){
+    row1.push(gameState[i].slice())
+  }
+  for(let i=3; i<6; i++){   
+    row2.push(gameState[i].slice())
+  }
+  for(let i=6; i<9; i++){
+    row3.push(gameState[i].slice())
+  }
+
+  const handleGameState = () => {
+    setGameState([row1[0], row1[1], row1[2], row2[0], row2[1], row2[2], row3[0], row3[1], row3[2]])
+  }
+  const moveCheck = () => {
+    const checkMoveState = checkXOrO(clickState);
+    setClickState(checkMoveState);
+  };
+
+
+  return (
     <div>
-      <Button />
+      <Button 
+        xOrOStatus={moveCheck}
+        player={getNewPlayer}
+        id="c1r1"        
+        >
+      </Button>
+      <Button
+        xOrOStatus={moveCheck}
+        player={getNewPlayer}
+        id="c1r2"
+        >
+      </Button>
+      <Button
+        xOrOStatus={moveCheck}
+        player={getNewPlayer}
+        id="c1r3"
+        >
+      </Button>
+      <br/>
+      <Button 
+        xOrOStatus={moveCheck}
+        player={getNewPlayer}
+        id="c2r1"
+        >
+      </Button>
+      <Button 
+        xOrOStatus={moveCheck}
+        player={getNewPlayer}
+        id="c2r2"
+        >
+      </Button>
+      <Button 
+        xOrOStatus={moveCheck}
+        player={getNewPlayer}
+        id="c2r3"
+        >
+      </Button>
+      <br/>
+      <Button 
+        xOrOStatus={moveCheck}
+        player={getNewPlayer}
+        id="c3r1"
+        >
+      </Button>
+      <Button 
+        xOrOStatus={moveCheck}
+        player={getNewPlayer}
+        id="c3r2"
+        >
+      </Button>
+      <Button 
+        xOrOStatus={moveCheck}
+        player={getNewPlayer}
+        id="c3r3"
+        >
+      </Button>
     </div>
-  )
-
+  );
 };
 
 export default GameWinCondition;
